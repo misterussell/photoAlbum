6fec6889d8eb00d5f81e905fc45449d8da0fb691
Maxwell Russell <maxruss.87@gmail.com>
2016-10-14 14:53:41 -0500

    basic array added

---
+++ images/images.js
@@ -0,0 +1,70 @@
+var albums = [
+  {
+    albumName: 'surfers',
+    albumURL: 'images/albumOne',
+    albumDescription: 'Images from the seaside.',
+    albumImages: [
+      {
+        imageName: 'Torrenting Ocean',
+        imageURL: 'images/albumOne/ocean1.jpg',
+      },{
+        imageName: 'Plumetting Wave',
+        imageURL: 'images/albumOne/ocean2.jpg',
+      },{
+        imageName: 'Palm Reflection',
+        imageURL: 'images/albumOne/ocean3.jpg',
+      },{
+        imageName: 'Helicopter Flight',
+        imageURL: 'images/albumOne/ocean4.jpg',
+      },{
+        imageName: 'Board Fin',
+        imageURL: 'images/albumOne/surfer1.jpg',
+      },{
+        imageName: 'In the Tube',
+        imageURL: 'images/albumOne/surfer2.jpg',
+      }
+    ],
+  },{
+      albumName: 'mountainViews',
+      albumURL: 'images/albumTwo',
+      albumDescription: 'Images from the mountains.',
+      albumImages: [
+        {
+          imageName: 'Misty Valley',
+          imageURL: 'images/albumTwo/Lake1.jpg',
+        },{
+          imageName: 'Stepe Mountain',
+          imageURL: 'images/albumTwo/mountain1.jpg',
+        },{
+          imageName: 'Snowy Mountaintop',
+          imageURL: 'images/albumTwo/mountain2.jpg',
+        },{
+          imageName: 'Desert Mountains',
+          imageURL: 'images/albumTwo/mountain3.jpg',
+        },{
+          imageName: 'Snowy Tree',
+          imageURL: 'images/albumTwo/mountain4.jpg',
+        },{
+          imageName: 'Sunset',
+          imageURL: 'images/albumTwo/mountain5.jpg',
+        }
+      ],
+    },
+];
+
+  // , {
+  //   albumName: 'mountains',
+  //   albumURL: 'images/albumTwo',
+  //   albumDescription: 'Images from the mountains.'
+  //
+  // }, {
+  //   albumName: 'ocean',
+  //   albumURL: 'images/albumThree',
+  //   albumDescription: 'Images from the deep sea.'
+  //
+  // }, {
+  //   albumName: 'valleys',
+  //   albumURL: '/images/albumFour',
+  //   albumDescription: 'Images from the valleys of the world.'
+  // }
+// ];
