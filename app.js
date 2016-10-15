c7b13093d04879c6a744eff3eb62e25f70bcbad6
Maxwell Russell <maxruss.87@gmail.com>
2016-10-15 12:47:48 -0500

    added in photo render

--- app.js
+++ app.js
@@ -1,36 +1,63 @@
 var sideMenu = $('.sideMenu');
 var pageHeader = $('.pageHeader');
 var contentBox = $('.selectedContent');
+var windowLocation = window.location.hash;
 
-function renderHome() {
+function renderHome(pageHeader, contentBox) {
   //change the header text
-  pageHeader.text('My Albums');
+  pageHeader.append('My Albums');
   //create a list item
   //add pictures to the list
   albums.forEach(function(album, i, arr){
-    // var $gridItem = $('<li>' + picture.previewImage + '</li>');
     var $gridItem = $('<a href="' + album.hashLocation + '"><li class="' + album.albumName + '"><img src="' + album.previewImage + '" /img><h2>' + album.albumDescription + '</h2></li></a>');
     contentBox.append($gridItem);
   });
 }
 
-function renderAlbum(sideMenu, pageHeader, contentBox) {
+function renderAlbum(sideMenu, pageHeader, contentBox, album) {
 
   //this will need to change to match the selected album
   pageHeader.text('Album');
 
   //get a specific album
-
-  //gets all images in album
   function getAlbum(album, i, arr) {
+    return album.albumName === 'surfers';
+  }
+  //gets all images in album
+  function getAlbumImages(album, i, arr) {
     return album.albumImages;
   }
+  //returns a defined album
+  var selectAlbum = albums.filter(getAlbum);
+  console.log(selectAlbum);
+  //returns only images - currently for all albums
+  var albumImages = albums.map(getAlbumImages);
+  console.log(albumImages);
 
-  var albumImages = albums.map(getAlbum);
-  // console.log(albumImages);
-
-
+  $('body').css('background', 'pink');
 }
 
-renderHome(pageHeader, contentBox);
-// renderAlbum(sideMenu, pageHeader, contentBox);
+function renderImage(pageHeader, contentBox) {
+  pageHeader.text('Photo');
+  console.log('hello');
+
+  $('body').css('background', '#6B949E');
+}
+
+// renderHome(pageHeader, contentBox);
+renderImage(pageHeader, contentBox);
+
+
+
+
+
+
+
+
+
+// window.onhashchange = changeContent();
+//
+// function changeContent() {
+//   // alert('hello');
+//   renderAlbum(sideMenu, pageHeader, contentBox);
+// }
