c55fd91383fa44b3ea761c55dc43edc3b4bfff96
Maxwell Russell <maxruss.87@gmail.com>
2016-10-16 23:04:27 -0500

    adjusted image specs

--- images/images.js
+++ images/images.js
@@ -2,198 +2,198 @@ var albums = [
   {
     albumName: 'surfers',
     albumURL: 'images/surfers',
-    previewImage: 'images/surfers/ocean2.jpeg',
+    previewImage: 'images/surfers/ocean2.jpg',
     albumDescription: 'Images from the seaside.',
     hashLocation: '#surferAlbum', //not currently using
     albumImages: [
       {
         imageName: 'Torrenting Ocean',
-        imageURL: 'images/surfers/ocean1.jpeg',
+        imageURL: 'images/surfers/ocean1.jpg',
         imageHash: '%ocean1',
       },{
         imageName: 'Plumetting Wave',
-        imageURL: 'images/surfers/ocean2.jpeg',
+        imageURL: 'images/surfers/ocean2.jpg',
         imageHash: '%ocean2',
       },{
         imageName: 'Palm Reflection',
-        imageURL: 'images/surfers/ocean3.jpeg',
+        imageURL: 'images/surfers/ocean3.jpg',
         imageHash: '%ocean3',
       },{
         imageName: 'Helicopter Flight',
-        imageURL: 'images/surfers/ocean4.jpeg',
+        imageURL: 'images/surfers/ocean4.jpg',
         imageHash: '%ocean4',
       },{
         imageName: 'Board Fin',
-        imageURL: 'images/surfers/surfer1.jpeg',
+        imageURL: 'images/surfers/surfer1.jpg',
         imageHash: '%ocean5',
       },{
         imageName: 'In the Tube',
-        imageURL: 'images/surfers/surfer2.jpeg',
+        imageURL: 'images/surfers/surfer2.jpg',
         imageHash: '%ocean6',
       }
     ],
   },{
     albumName: 'mountainViews',
     albumURL: 'images/mountainViews',
-    previewImage: 'images/mountainViews/Lake1.jpeg',
+    previewImage: 'images/mountainViews/Lake1.jpg',
     albumDescription: 'Images from the mountains.',
     hashLocation: '#mountainAlbum',
     albumImages: [
       {
         imageName: 'Misty Valley',
-        imageURL: 'images/mountainViews/Lake1.jpeg',
+        imageURL: 'images/mountainViews/Lake1.jpg',
         imageHash: '%mountain1',
       },{
         imageName: 'Stepe Mountain',
-        imageURL: 'images/mountainViews/mountain1.jpeg',
+        imageURL: 'images/mountainViews/mountain1.jpg',
         imageHash: '%mountain2',
       },{
         imageName: 'Snowy Mountaintop',
-        imageURL: 'images/mountainViews/mountain2.jpeg',
+        imageURL: 'images/mountainViews/mountain2.jpg',
         imageHash: '%mountain3',
       },{
         imageName: 'Desert Mountains',
-        imageURL: 'images/mountainViews/mountain3.jpeg',
+        imageURL: 'images/mountainViews/mountain3.jpg',
         imageHash: '%mountain4',
       },{
         imageName: 'Snowy Tree',
-        imageURL: 'images/mountainViews/mountain4.jpeg',
+        imageURL: 'images/mountainViews/mountain4.jpg',
         imageHash: '%mountain5',
       },{
         imageName: 'Sunset',
-        imageURL: 'images/mountainViews/mountain5.jpeg',
+        imageURL: 'images/mountainViews/mountain5.jpg',
         imageHash: '%mountain6',
       }
     ],
   },{
     albumName: 'deepSea',
     albumURL: 'images/deepSea',
-    previewImage: 'images/deepSea/jellyfish1.jpeg',
+    previewImage: 'images/deepSea/jellyfish1.jpg',
     albumDescription: 'Images from the sea.',
     hashLocation: '#seaAlbum',
     albumImages: [
       {
         imageName: 'Sunning Jellyfish',
-        imageURL: 'images/deepSea/jellyfish1.jpeg',
+        imageURL: 'images/deepSea/jellyfish1.jpg',
         imageHash: '%sea1',
       },{
         imageName: 'Reef School',
-        imageURL: 'images/deepSea/school1.jpeg',
+        imageURL: 'images/deepSea/school1.jpg',
         imageHash: '%sea2',
       },{
         imageName: 'Silver School',
-        imageURL: 'images/deepSea/school2.jpeg',
+        imageURL: 'images/deepSea/school2.jpg',
         imageHash: '%sea3',
       },{
         imageName: 'Wandering Frenzy',
-        imageURL: 'images/deepSea/sharks.jpeg',
+        imageURL: 'images/deepSea/sharks.jpg',
         imageHash: '%sea4',
       },{
         imageName: 'Night Hunter',
-        imageURL: 'images/deepSea/sharks2.jpeg',
+        imageURL: 'images/deepSea/sharks2.jpg',
         imageHash: '%sea5',
       },{
         imageName: 'Old Eyes',
-        imageURL: 'images/deepSea/turtle2.jpeg',
+        imageURL: 'images/deepSea/turtle2.jpg',
         imageHash: '%sea6',
       }
     ],
   },{
     albumName: 'valleys',
     albumURL: 'images/valleys',
-    previewImage: 'images/valleys/valley1.jpeg',
+    previewImage: 'images/valleys/valley1.jpg',
     albumDescription: 'Images from the valleys of the world.',
     hashLocation: '#valleyAlbum',
     albumImages: [
       {
         imageName: 'Night Sky',
-        imageURL: 'images/valleys/space1.jpeg',
+        imageURL: 'images/valleys/space1.jpg',
         imageHash: '%valley1',
       },{
         imageName: 'Valley Hills',
-        imageURL: 'images/valleys/valley1.jpeg',
+        imageURL: 'images/valleys/valley1.jpg',
         imageHash: '%valley2',
       },{
         imageName: 'Sunset',
-        imageURL: 'images/valleys/valley2.jpeg',
+        imageURL: 'images/valleys/valley2.jpg',
         imageHash: '%valley3',
       },{
         imageName: 'River Valley',
-        imageURL: 'images/valleys/valley3.jpeg',
+        imageURL: 'images/valleys/valley3.jpg',
         imageHash: '%valley4',
       },{
         imageName: 'Valley Town',
-        imageURL: 'images/valleys/valley4.jpeg',
+        imageURL: 'images/valleys/valley4.jpg',
         imageHash: '%valley5',
       },{
         imageName: 'Windfarm',
-        imageURL: 'images/valleys/windfarm1.jpeg',
+        imageURL: 'images/valleys/windfarm1.jpg',
         imageHash: '%valley6',
       }
     ],
   },{
     albumName: 'insects',
     albumURL: 'images/insects',
-    previewImage: 'images/insects/grasshopper1.jpeg',
+    previewImage: 'images/insects/grasshopper1.jpg',
     albumDescription: 'Images of Earths smaller creatures.',
     hashLocation: '#insectAlbum',
     albumImages: [
       {
         imageName: 'Perched Bee',
-        imageURL: 'images/insects/bee1.jpeg',
+        imageURL: 'images/insects/bee1.jpg',
         imageHash: '%insect1',
       },{
         imageName: 'Perched Dragonfly',
-        imageURL: 'images/insects/dragonfly1.jpeg',
+        imageURL: 'images/insects/dragonfly1.jpg',
         imageHash: '%insect2',
       },{
         imageName: 'Traveling Ladybug',
-        imageURL: 'images/insects/ladybug1.jpeg',
+        imageURL: 'images/insects/ladybug1.jpg',
         imageHash: '%insect3',
       },{
         imageName: 'Forest Snail',
-        imageURL: 'images/insects/snail1.jpeg',
+        imageURL: 'images/insects/snail1.jpg',
         imageHash: '%insect4',
       },{
         imageName: 'Little Spider',
-        imageURL: 'images/insects/spider1.jpeg',
+        imageURL: 'images/insects/spider1.jpg',
         imageHash: '%insect5',
       },{
         imageName: 'Perched Grasshopper',
-        imageURL: 'images/insects/grasshopper1.jpeg',
+        imageURL: 'images/insects/grasshopper1.jpg',
         imageHash: '%insect6',
       }
     ],
   },{
     albumName: 'elephants',
     albumURL: 'images/elephants',
-    previewImage: 'images/elephants/elephant1.jpeg',
+    previewImage: 'images/elephants/elephant1.jpg',
     albumDescription: 'Images of Earths larger creatures.',
     hashLocation: '#elephantAlbum',
     albumImages: [
       {
         imageName: 'Ancient Mud',
-        imageURL: 'images/elephants/elephant1.jpeg',
+        imageURL: 'images/elephants/elephant1.jpg',
         imageHash: '%elephant1',
       },{
         imageName: 'Bathing Child',
-        imageURL: 'images/elephants/elephant2.jpeg',
+        imageURL: 'images/elephants/elephant2.jpg',
         imageHash: '%elephant2',
       },{
         imageName: 'Night Travelers',
-        imageURL: 'images/elephants/elephant3.jpeg',
+        imageURL: 'images/elephants/elephant3.jpg',
         imageHash: '%elephant3',
       },{
         imageName: 'Sunset Walk',
-        imageURL: 'images/elephants/elephant4.jpeg',
+        imageURL: 'images/elephants/elephant4.jpg',
         imageHash: '%elephant4',
       },{
         imageName: 'Mother and Child',
-        imageURL: 'images/elephants/elephant5.jpeg',
+        imageURL: 'images/elephants/elephant5.jpg',
         imageHash: '%elephant5',
       },{
         imageName: 'A New Day',
-        imageURL: 'images/elephants/elephant6.jpeg',
+        imageURL: 'images/elephants/elephant6.jpg',
         imageHash: '%elephant6',
       }
     ],
